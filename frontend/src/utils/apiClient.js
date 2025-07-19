60c3b27f140e343c341ce50a8bc2964d5ce5f9e4
aayush-rai12 <ayush.wrk1212@gmail.com>
2025-07-19 08:51:08 +0530

    Update apiClient.js

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://skyserve-task-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
